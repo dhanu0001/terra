e673d5de6a53eb35b543063b0c9df11aa5fdb29f
Reddy <anju003@ymail.com>
2022-01-23 03:07:38 -0800

    newfiles

---
+++ terraform-variables/createinstance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "myfirstinstance" {
+    count = 3
+    ami = "ami-0f2e255ec956ade7f"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "demoinstance-${count.index}"
+    }
+}
